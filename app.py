5f168c7465bf69595273e510da7c12f0627dd4a9
Abhiram-python <gurujalaabhiram@gmail.com>
2026-07-03 20:08:03 +0530

    c

--- app.py
+++ app.py
@@ -83,7 +83,10 @@ class OrderFlowCollector:
     def fetch_initial_snapshot(self):
         print("[SYSTEM] Fetching initial order book snapshot...")
         try:
-            res = requests.get(REST_URL_DEPTH).json()
+            req=requests.get(REST_URL_DEPTH)
+            res = req.json()
+            print(req.status_code)
+            print(req.text)
             for b in res['bids']: self.bids[float(b[0])] = float(b[1])
             for a in res['asks']: self.asks[float(a[0])] = float(a[1])
             print("[SYSTEM] Snapshot loaded successfully.")
